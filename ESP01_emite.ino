0aceeb5517c7fc9a10e282da2df0288c8140dfbd
jtoobe <125473983+jtoobe@users.noreply.github.com>
2023-09-06 13:08:57 -0300

    Update ESP01_emite.ino

--- ESP01_emite.ino
+++ ESP01_emite.ino
@@ -1,30 +1,26 @@
 /*
 * Para LOLIN D1 mini clone, FUNCIONA TAMBIEN CON esp01
-* Envia datos cuando se conecta a la fuente
-* Como esta lo hace cada 10 segundos, 
-* lo tendria que hacer una vez sola  
+* Envia un dato cada 20 segundos
 */
  
 #include <ESP8266WiFi.h>
 #include <espnow.h>
  
-// REPLACE WITH RECEIVER MAC Address
-// macaddress ESP01 pcb azul (original)
+// Reemplazar con la MAC Address del ESP que recibe
 uint8_t broadcastAddress[] = {0x18, 0xFE, 0x34, 0xF3, 0x44, 0xE3};
-// macaddress ESP8266 1 marca
-//uint8_t broadcastAddress[] = {0x8C, 0xCE, 0x4E, 0xCE, 0x92, 0x23};
+
 int cuenta=0;
-// Structure example to send data
-// Must match the receiver structure
+// Creo una estructura para enviar los datos
+// esta debe estar replicada en el receptor
 typedef struct struct_message {
 char palabra[6];
   int numero;
 } struct_message;
  
-// Create a struct_message called myData
+// Creo la estructura para enviar los datos
 struct_message myData;
  
-// Callback when data is sent
+// Funcion que se ejecuta para enviar el dato
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Mensaje enviado: ");
   if (sendStatus == 0){
@@ -38,33 +34,32 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 }
   
 void setup() {
-  // Init Serial Monitor
+  
   Serial.begin(115200);
   
-  // Set device as a Wi-Fi Station
+  // Defino el funcionamiento del ESP01 como Wi-Fi Station
   WiFi.mode(WIFI_STA);
  
   // Init ESP-NOW
   if (esp_now_init() != 0) {
-    Serial.println("Error initializing ESP-NOW");
+    Serial.println("Error inicializando ESP-NOW");
     return;
   }
  
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+  
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
    
-  // Register peer
+  // Registro el receptor
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
   
 void loop() {
-  // Set values to send
+  // Valores a enviar
   strcpy(myData.palabra, "39552");
   myData.numero =5;
    
-  // Send message via ESP-NOW
+  // envio el mensaje via ESP-NOW
   esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
  
   delay(20000);
